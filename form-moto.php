6ddb3006f590d3d32b908a660f190643b3beacd1
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-27 14:54:21 -0300

    Create form-moto.php

---
+++ form-moto.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiMotos</title>
+</head>
+<body>
+    <form action="enviar-form-moto.php" method="get">
+        digite o nome da moto <br>
+        <input type="text" name="nome"> <br>
+        digite as especificações da moto <br>
+        <input type="text" name="especificacoes"> <br>
+        digite o preço da moto <br>
+        <input type="text" name="preco"> <br>
+        digite a imagem da moto <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
