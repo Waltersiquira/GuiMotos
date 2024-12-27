52b88102ebb1d3f4dc8eb700edd972cc5f467019
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2024-12-27 14:50:27 -0300

    Create comprar-moto.php

---
+++ comprar-moto.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiMotos</title>
+</head>
+<body style="text-align: center;">
+    <h1>Aprovado</h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqi1o2JZ4DMpPe3ZT34rY9P4jE8tSC3EeGZw&s" alt="" width="550">
+    <p>Moto comprada com sucesso</p>
+</body>
+</html>
